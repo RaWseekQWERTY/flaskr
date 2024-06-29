61605e47b4965d0efb8dba39a10db92461fe279b
rasik <rasikkayasthav@gmail.com>
2024-06-29 21:04:41 +0545

    blog logics

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -2,5 +2,16 @@ from flask import(
     Blueprint, views, flash, g, render_template, redirect, session, request,url_for
 )
 from flaskr.db import get_DB
+from flaskr.auth import login_required
 
-bp = Blueprint("blog".__name__, url_prefix="/blog")
+bp = Blueprint("blog".__name__)
+
+@bp.route('/')
+def index():
+    db = get_DB()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts)
